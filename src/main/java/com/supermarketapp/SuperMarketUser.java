8b15c6c39fc6ffd9f338f169348606193874fd48
spriyam1221 <spriyam1221@gmail.com>
2022-03-15 21:37:52 +0530

    Add the user moble test the test case

---
+++ src/main/java/com/supermarketapp/SuperMarketUser.java
@@ -0,0 +1,54 @@
+package com.supermarketapp;
+
+public class SuperMarketUser {
+ 
+	private  Integer CustomerId;
+	private String name;
+	private String email;
+	private String password;
+	private String mobileNumber;
+	private char gender;
+	public Integer getCustomerId() {
+		return CustomerId;
+	}
+	public  void setCustomerId(Integer customerId) {
+		this.CustomerId = customerId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getMobileNumber() {
+		return mobileNumber;
+	}
+	public void setMobileNumber(String mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+	public char getGender() {
+		return gender;
+	}
+	public void setGender(char gender) {
+		this.gender = gender;
+	}
+	@Override
+	public String toString() {
+		return "SuperMarketAppUser [CustomerId=" + CustomerId + ", name=" + name + ", email=" + email + ", password="
+				+ password + ", mobileNumber=" + mobileNumber + ", gender=" + gender + "]";
+	}
+	
+	
+}
---
+++ src/main/java/com/supermarketapp/TestSuperMarketUser.java
@@ -0,0 +1,17 @@
+package com.supermarketapp;
+
+public class TestSuperMarketUser {
+
+	public static void main(String[] args) {
+	SuperMarketUser s1 = new SuperMarketUser();
+	//	SuperMarketUser superMarketUser = new SuperMarketUser();
+		s1.setCustomerId(1);
+		s1.setName("priya");
+		s1.setEmail("pri@gmail.com");
+		s1.setMobileNumber("8973279683");
+		s1.setGender('F');
+		System.out.println(s1);
+		
+	}
+
+}
